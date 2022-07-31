321fe69c716f1294e0ef3c8d5ceb325403a60a4a
MetallicGoat <the.great.metallic.goat@gmail.com>
2022-07-30 23:49:52 -0400

    Redo item placement logic (Still needs work)

--- src/main/java/me/metallicgoat/hotbarmanageraddon/OnShopBuy.java
+++ src/main/java/me/metallicgoat/hotbarmanageraddon/OnShopBuy.java
@@ -2,6 +2,7 @@ package me.metallicgoat.hotbarmanageraddon;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import de.marcely.bedwars.api.GameAPI;
 import de.marcely.bedwars.api.arena.Arena;
 import de.marcely.bedwars.api.event.player.PlayerBuyInShopEvent;
 import de.marcely.bedwars.api.game.shop.ShopItem;
@@ -24,6 +25,15 @@ import java.util.Optional;
 public class OnShopBuy implements Listener {
 
     // TODO Simplify this mess
+    // TODO add support for shopItems with multiple products
+
+
+    /*
+     * 1. Try to put item in slot with similar itemstack
+     * 2. Try to put item in null slot for that category
+     * 3. Try and force move an item
+     * 4. Give up. Add Item to inventory normally.
+     */
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onShopBuy(PlayerBuyInShopEvent event) {
@@ -37,107 +47,65 @@ public class OnShopBuy implements Listener {
         // TODO check if category is enabled
 
         final ItemStack givenItem = getGivingItem(event.getItem(), player, event.getArena());
+
+        giveItemsProperly(givenItem, player, page, event);
+
+
+    }
+
+    private void giveItemsProperly(ItemStack givenItem, Player player, ShopPage page, PlayerBuyInShopEvent event){
+
+        // We will handle giving the items
+        if(event != null)
+            event.setGivingProducts(false);
+
         final Integer slot = getPreferredSlot(givenItem.clone(), page, player);
-
-        if (slot == null)
-            return;
-
         final Inventory inventory = player.getInventory();
 
-
-        event.setGivingProducts(false);
-
-        if (inventory.getItem(slot) == null)
+        if (slot == null) {
             inventory.addItem(givenItem);
+            return;
+        }
+
+        final ItemStack currItemInSlot = inventory.getItem(slot);
+
+        // Yay! The slot we want to use is already empty
+        if (currItemInSlot == null)
+            inventory.setItem(slot, givenItem);
 
         else {
+            if (currItemInSlot.isSimilar(givenItem)) {
 
-            final ItemStack is = inventory.getItem(slot).clone();
+                final int total = currItemInSlot.getAmount() + givenItem.getAmount();
+                final int maxStack = currItemInSlot.getMaxStackSize();
 
-            // TODO make better
-            if (is.isSimilar(givenItem)) {
-                final int total = is.getAmount() + givenItem.getAmount();
-                final int maxStack = is.getMaxStackSize();
-
-                if(total > maxStack){
+                // To many to fit in one slot
+                if (total > maxStack) {
                     givenItem.setAmount(maxStack);
                     inventory.setItem(slot, givenItem);
-                    addExtra(givenItem, total - maxStack, inventory);
-                }else{
+
+                    // Keep redoing this until all items given
+                    final ItemStack leftOver = givenItem.clone();
+                    leftOver.setAmount(total - maxStack);
+
+                    giveItemsProperly(leftOver, player, page, null);
+                }
+
+                // Yay! We don't need to find more space! Everything fits
+                else {
                     givenItem.setAmount(total);
                     inventory.setItem(slot, givenItem);
                 }
 
             } else {
+                // Force move an item
                 inventory.setItem(slot, givenItem);
-                addItem(is, inventory);
+                inventory.addItem(currItemInSlot.clone());
             }
         }
     }
 
-    public void addExtra(ItemStack given, int amount, Inventory inventory){
-
-        final int maxStack = given.getMaxStackSize();
-
-        while(amount > 0){
-
-            final int giveAmount = Math.min(amount, maxStack);
-            final ItemStack givenStack = given.clone();
-
-            givenStack.setAmount(giveAmount);
-            addItem(givenStack, inventory);
-
-            amount = amount - giveAmount;
-        }
-    }
-
-    public void addItem(ItemStack itemStack, Inventory inventory){
-
-        int slot = 0;
-        int amount = itemStack.getAmount();
-
-        while(slot < 36){
-            final ItemStack currentStack = inventory.getItem(slot);
-
-            if(currentStack != null && currentStack.isSimilar(itemStack)){
-
-                final int currAmount = currentStack.getAmount();
-                final int maxStack = currentStack.getMaxStackSize();
-                final ItemStack given = itemStack.clone();
-                given.setAmount(Math.min(maxStack, amount + currAmount));
-                amount = amount - (maxStack - currAmount);
-
-                inventory.setItem(slot, given);
-
-                if(amount < 1)
-                    return;
-
-            }
-
-            slot++;
-        }
-
-        slot = 9;
-
-        final ItemStack given = itemStack.clone();
-        given.setAmount(amount);
-
-        while(slot < 36){
-            final ItemStack currentStack = inventory.getItem(slot);
-
-            if(currentStack == null) {
-                inventory.setItem(slot, given);
-                return;
-            }
-
-            slot++;
-        }
-
-        // TODO Possible items will be lost after inventory full
-        inventory.addItem(given);
-    }
-
-    private static ItemStack getGivingItem(ShopItem item, Player player, Arena arena) {
+    private ItemStack getGivingItem(ShopItem item, Player player, Arena arena) {
 
         for (ShopProduct product : item.getProducts()) {
 
@@ -150,7 +118,7 @@ public class OnShopBuy implements Listener {
         return new ItemStack(Material.AIR);
     }
 
-    private static Integer getPreferredSlot(ItemStack givenItem, ShopPage page, Player player) {
+    private Integer getPreferredSlot(ItemStack givenItem, ShopPage page, Player player) {
 
         final Optional<PlayerProperties> propertiesOptional = PlayerDataAPI.get().getPropertiesNow(player.getUniqueId());
 
@@ -170,8 +138,8 @@ public class OnShopBuy implements Listener {
 
         final Inventory inventory = player.getInventory();
         final String category = page.getName();
-        Integer backupSlot = null;
 
+        // Try to find similar slot
         for (Map.Entry<Integer, String> entry : layout.entrySet()) {
 
             if (!entry.getValue().equals(category))
@@ -179,15 +147,73 @@ public class OnShopBuy implements Listener {
 
             final ItemStack currStack = inventory.getItem(entry.getKey());
 
-            if (currStack == null || (currStack.isSimilar(givenItem) && currStack.getAmount() < currStack.getMaxStackSize()))
+            if (currStack != null && currStack.isSimilar(givenItem) && currStack.getAmount() < currStack.getMaxStackSize())
                 return entry.getKey();
-
-            if (backupSlot == null)
-                backupSlot = entry.getKey();
         }
 
-        // TODO offer empty hotbar slots
+        // Try to find empty slot
+        for (Map.Entry<Integer, String> entry : layout.entrySet()) {
 
-        return backupSlot;
+            System.out.println(1);
+
+            if (!entry.getValue().equals(category))
+                continue;
+
+            System.out.println(2);
+            final ItemStack currStack = inventory.getItem(entry.getKey());
+
+            if (currStack == null) {
+                System.out.println(3);
+                return entry.getKey();
+            }
+        }
+
+        // Check if we can force move any items
+        if(isHotbarFull(inventory)) {
+            for (Map.Entry<Integer, String> entry : layout.entrySet()) {
+
+                if (!entry.getValue().equals(category))
+                    continue;
+
+                final ItemStack currStack = inventory.getItem(entry.getKey());
+
+                if (!isItemInSameCategory(page, currStack, player))
+                    return entry.getKey();
+            }
+        }
+
+        return null;
+    }
+
+    private boolean isItemInSameCategory(ShopPage page, ItemStack slotStack, Player player){
+
+        final Arena arena = GameAPI.get().getArenaByPlayer(player);
+
+        if(arena == null)
+            return false;
+
+        for(ShopItem item : page.getItems()){
+            for(ShopProduct product : item.getProducts()){
+                for(ItemStack itemStack : product.getGivingItems(player, arena.getPlayerTeam(player), arena, 1)){
+                    if(itemStack.isSimilar(slotStack))
+                        return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean isHotbarFull(Inventory inventory){
+
+        int i = 0;
+
+        while(i < 9){
+            if(inventory.getItem(i) == null)
+                return false;
+
+            i++;
+        }
+        return true;
     }
 }
